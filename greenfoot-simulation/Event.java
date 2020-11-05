310ec636bffa8f728c20ff76b8edd01075f725f7
hpnrep6 <57055412+hpnrep6@users.noreply.github.com>
2020-11-05 14:16:11 -0500

    Add files via upload

--- greenfoot-simulation/Event.java
+++ greenfoot-simulation/Event.java
@@ -12,6 +12,10 @@ public abstract class Event extends Actor
     METEOR = 0,
     TORNADO = 1;
     
+    protected static final int DEFAULT_DAMAGE = 25;
+    
+    protected int damage, range;
+    
     protected int xLoc, yLoc, rot;
     
     public int getX() {
@@ -25,8 +29,8 @@ public abstract class Event extends Actor
     /**
      * Get the buildings within specified range
      * 
-     * @param x Location in x axis of location to search from
-     * @param y Location in y axis of location to search from
+     * @param x Location in x axis to search from
+     * @param y Location in y axis to search from
      * @param range Range to search for buildings
      */
     
@@ -46,8 +50,8 @@ public abstract class Event extends Actor
     /**
      * Get the humans within specified range
      * 
-     * @param x Location in x axis of location to search from
-     * @param y Location in y axis of location to search from
+     * @param x Location in x axis to search from
+     * @param y Location in y axis to search from
      * @param range Range to search for humans
      */
     protected ArrayList<Human> getHumansWithinRange(int x, int y, int range) {
@@ -61,6 +65,53 @@ public abstract class Event extends Actor
             }
         }
         return list;
+    }
+    
+    
+    /**
+     * Get the trees within specified range
+     * 
+     * @param x Location in x axis to search from
+     * @param y Location in y axis to search from
+     * @param range Range to search for trees
+     */
+    protected ArrayList<Tree> getTreesWithinRange(int x, int y, int range) {
+        ArrayList<Tree> list = new ArrayList<Tree>();
+        for(int i = 0, n = WorldManagement.trees.size(); i < n; i++) {
+            Tree tree = (Tree)(WorldManagement.trees.get(i));
+            int xLoc = tree.getX(), yLoc = tree.getY();
+            int distance = Utils.calcDist(x, xLoc, y, yLoc);  
+            if(distance <= range) {
+                list.add(tree);
+            }
+        }
+        return list;
+    }
+    
+    /**
+     * Damages the nearby humans, trees, and buildings
+     * 
+     * @param xLoc Location in x axis to search for objects that are to be damaged from 
+     * @param yLoc Location in y axis to search for objects that are to be damaged from 
+     * @param range The range of the object search
+     * @param damage How much to damage the objects found
+     */
+    
+    protected void killNearbyThings(int xLoc, int yLoc, int range, int damage) {
+        ArrayList<BuildingSlot> buildings = getBuildingsWithinRange(xLoc, yLoc, range);
+        for(int i = 0; i < buildings.size(); i++) {
+            ((BuildingSlot)buildings.get(i)).damage(damage);
+        }
+        
+        ArrayList<Human> humans = getHumansWithinRange(xLoc, yLoc, range);
+        for(int i = 0; i < humans.size(); i++) {
+            ((Human)humans.get(i)).damage(damage);
+        }
+        
+        ArrayList<Tree> trees = getTreesWithinRange(xLoc, yLoc, range);
+        for(int i = 0; i < trees.size(); i++) {
+            ((Tree)trees.get(i)).destroy();
+        }
     }
     
     public abstract void _update();
--- greenfoot-simulation/Meteor.java
+++ greenfoot-simulation/Meteor.java
@@ -8,12 +8,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Meteor extends Event
 {
-    /**
-     * Act - do whatever the Meteor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private static final int MIN_DIST = 200, MAX_DIST = 300 + MIN_DIST;
+    private static final int SPEED = 5;
+    
+    private int targetY, targetX;
+    
+    private float angleToTarget;
+    
+    public Meteor(int x, int y) {
+        GreenfootImage image = new GreenfootImage("tornado.png");
+        setImage(image);
+        
+        targetX = x;
+        targetY = y;
+        
+        float randomRotation = (float)(Math.random() * 29393f);
+        
+        int distanceFromTarget = (int)((randomRotation * 1312) % MAX_DIST) + MIN_DIST;
+        
+        this.xLoc = x + (int) (Math.cos(randomRotation) * distanceFromTarget);
+        this.yLoc = y + (int) (Math.sin(randomRotation) * distanceFromTarget);
+       
+        angleToTarget = (float) Math.atan2( (float) (y - yLoc), (float) (x - xLoc));
+        
+        this.rot = (int) (angleToTarget % (Math.PI * 2));
+        
+        throw new Error("We no have meteor");
+    }
     public void _update() 
     {
-        // Add your action code here.
+        xLoc += (int) (Math.cos(angleToTarget) * SPEED);
+        yLoc += (int) (Math.sin(angleToTarget) * SPEED);
+        setRotation(rot);
     }    
 }
